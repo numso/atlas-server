97aedd26afb765864b94caf3a54389c1c0ee8788
numso <dosmun@gmail.com>
2014-06-25 22:45:24 -0600

    added restart script

---
+++ restart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        export PATH=/usr/local/bin:$PATH
+        cd /opt/nodejs/atlas
+        forever start -a --uid atlas -c "npm start" ./
+fi
